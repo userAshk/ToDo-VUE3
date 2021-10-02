92d2deab84a77525dff4c65482521a2bb9a904fa
userAshk <blvckm8@gmail.com>
2021-10-02 16:17:34 +0300

    done

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ Vue.createApp({
     data(){
         return{
             valueInput: "",
-            needDoList: []
+            needDoList: [],
+            completeList: []
         };
     },
     methods:{
@@ -16,6 +17,19 @@ Vue.createApp({
                 id: Math.random()
             });
             this.valueInput = '';
+        },
+        doCheck (index, type) {
+            if(type === "need"){
+                const completeMask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeMask);
+            }else{
+                const noCompleteMask = this.completeList.splice(index, 1);
+                this.needDoList.push(...noCompleteMask)
+            }
+        },
+        removeMask(index, type){
+            const toDoList = type === 'need' ? this.needDoList : this.completeList;
+            toDoList.splice(index, 1);
         }
     }
 }).mount('#app');
